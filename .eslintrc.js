02c625688fa0bb5ebdfa164d3abfe8af8c464893
wjxu <906626481@qq.com>
2021-02-19 11:17:07 +0800

    fix: fix some config issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
   rules: {
+    "prettier/prettier":"warn",
     "react/react-in-jsx-scope": "error",
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": "error",
